113a6ab84c42f4246d43fa1dc0764035a7f19ae4
Ruby-Adhana <145450062+Ruby-Adhana@users.noreply.github.com>
2024-06-29 11:31:11 +0530

    Update root-file.sh

--- root-file.sh
+++ root-file.sh
@@ -1,8 +1,17 @@
 sudo passwd root
-cd /etc/gdm3
-sudo gedit custom.conf
+sudo gedit /etc/gdm3/custom.conf
 AllowRoot=true    <--this row  No. 17 
 
-gedit gdm-password
-sudo gedit gdm-password
+sudo gedit /etc/pam.d/gdm-password
 # <command 3 row >
+
+#install sshd server
+apt install openssh-server -y
+apt install vim -y
+apt install apache2 -y
+apt install firewalld -y
+apt update -y
+
+#root access sudoers 
+vim /etc/ssh/sshd_config
+# 40 and 65 row
