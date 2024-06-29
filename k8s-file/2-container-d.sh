92c32976dec48abcf50590e5a4914e49bfc718f3
Ruby-Adhana <145450062+Ruby-Adhana@users.noreply.github.com>
2024-06-29 11:54:44 +0530

    Update 2-container-d.sh

--- k8s-file/2-container-d.sh
+++ k8s-file/2-container-d.sh
@@ -1,13 +1,13 @@
-sudo apt-get update 
-sudo apt-get install ca-certificates curl 
-sudo install -m 0755 -d /etc/apt/keyrings 
+sudo apt-get update -y
+sudo apt-get install ca-certificates curl -y
+sudo install -m 0755 -d /etc/apt/keyrings -y
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc 
 sudo chmod a+r /etc/apt/keyrings/docker.asc 
 echo \ 
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \ 
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \ 
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null 
-sudo apt-get update 
+sudo apt-get update -y
 sudo apt-get install containerd.io -y 
 containerd config default | sudo tee /etc/containerd/config.toml | sudo sed -i 's/SystemdCgroup = false/SystemdCgroup = true/' /etc/containerd/config.toml 
 sudo systemctl restart containerd 
